95fe1f52a00904238e06eeb4fba5ac5045bfa8fc
Michael Wilson <miwilson@sovrn.com>
2017-06-26 14:47:44 -0600

    Completed Exercise 6

--- Chapter_6_Exercise_1.js
+++ Chapter_6_Exercise_1.js
@@ -1,8 +1,8 @@
 /*
 Chapter 6, Exercise 1: A Vector Type
 
-Write a constructor Vector that represents a vector in two-dimensional space. It takes x and y parameters (numbers), 
-which it should save to properties of the same name.
+Write a constructor Vector that represents a vector in two-dimensional space. It takes x and y parameters
+(numbers), which it should save to properties of the same name.
 
 Give the Vector prototype two methods, plus and minus, that take another vector as a parameter and return a new 
 vector that has the sum or difference of the two vectors’ (the one in this and the parameter) x and y values.
@@ -14,13 +14,28 @@ the distance of the point (x, y) from the origin (0, 0).
 
 var Vector = function(x, y){
 	this.x = x,
-	this.y = y,
-	get vLength(){
-		return Math.sqrt((this.x * this.x) + (this.y * this.y));
-	}
-
+	this.y = y
 }
 
-var newVector = new Vector(3, 4);
+Object.defineProperty(Vector.prototype, "length", {
+	get: function() {return Math.sqrt((this.x * this.x) + (this.y * this.y))}
+})
 
-console.log(newVector);
+Vector.prototype.plus = function(vObj){
+	var newX = this.x + vObj.x;
+	var newY = this.y + vObj.y;
+	return new Vector(newX, newY);
+};
+
+Vector.prototype.minus = function(vObj){
+	var newX = this.x - vObj.x;
+	var newY = this.y - vObj.y;
+	return new Vector(newX, newY);
+};
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
